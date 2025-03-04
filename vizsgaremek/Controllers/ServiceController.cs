dd076559a2e584858e4254a6fb16ff562859a258
budahazim@kkszki.hu <budahazim@kkszki.hu>
2025-03-04 12:34:50 +0100

    A szolgáltatások most már tartalmazzák a feltöltő felhasználónevét is

--- vizsgaremek/Controllers/ServiceController.cs
+++ vizsgaremek/Controllers/ServiceController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Org.BouncyCastle.Asn1.Ocsp;
 using Org.BouncyCastle.Bcpg;
+using System.Linq.Expressions;
 using vizsgaremek.DTOs;
 using vizsgaremek.Models;
 
@@ -66,23 +67,38 @@ namespace vizsgaremek.Controllers
             {
                 try
                 {
+                    var keres = context.Services
+                        .Where(x => x.UserId == id)
+                        .Select(service => new
+                        {
+                            service.ServiceId,
+                            service.UserId,
+                            service.ServiceName,
+                            service.TimeCost,
+                            service.Description,
+                            service.CreatedAt,
+                            service.CategoryId,
+                            FelhasznaloNev = context.Users
+                                .Where(user => user.UserId == service.UserId)
+                                .Select(user => user.FelhasznaloNev)
+                                .FirstOrDefault()
+                        })
+                        .ToList();
 
-                    var keres = context.Services.Where(x => x.UserId == id).ToList();
                     if (keres == null || keres.Count == 0)
                     {
                         return NotFound("Nem találtunk szolgáltatást");
                     }
-                    else { return Ok(keres); }
 
-
+                    return Ok(keres);
                 }
                 catch (Exception ex)
                 {
-
                     return BadRequest(ex.Message);
                 }
             }
         }
+
         [HttpGet("AllService")]
         public IActionResult GetUsers()
         {
@@ -111,112 +127,126 @@ namespace vizsgaremek.Controllers
             {
                 try
                 {
+                    var keres = context.Services
+                        .Where(x => x.ServiceId == id)
+                        .Select(service => new
+                        {
+                            service.ServiceId,
+                            service.UserId,
+                            service.ServiceName,
+                            service.TimeCost,
+                            service.Description,
+                            service.CreatedAt,
+                            service.CategoryId,
+                            FelhasznaloNev = context.Users
+                                .Where(user => user.UserId == service.UserId)
+                                .Select(user => user.FelhasznaloNev)
+                                .FirstOrDefault()
+                        })
+                        .ToList();
 
-                    var keres = context.Services.FirstOrDefault(x => x.ServiceId == id);
-                    if (keres == null)
+                    if (keres == null || keres.Count == 0)
                     {
-                        return NotFound("Még nincs ilyen szolgáltatás ezzel az azonosítóval");
+                        return NotFound("Nem találtunk szolgáltatást");
                     }
-                    else { return Ok(keres); }
-
+                    return Ok(keres);
 
                 }
                 catch (Exception ex)
                 {
-
                     return BadRequest(ex.Message);
                 }
             }
         }
-        [HttpDelete("/{serviceId}")]
-        public async Task<IActionResult> DeleteService(int serviceId, string uId)
-        {
-            using (var context = new VizsgaremekContext())
+            [HttpDelete("/{serviceId}")]
+            public async Task<IActionResult> DeleteService(int serviceId, string uId)
             {
-                try
+                using (var context = new VizsgaremekContext())
                 {
-                    if (!Program.LoggedInUsers.ContainsKey(uId))
+                    try
                     {
-                        return Unauthorized("Nem vagy bejelentkezve");
+                        if (!Program.LoggedInUsers.ContainsKey(uId))
+                        {
+                            return Unauthorized("Nem vagy bejelentkezve");
+                        }
+
+                        var loggedInUser = Program.LoggedInUsers[uId];
+                        int userId = Program.LoggedInUsers[uId].UserId;
+                        int userPermissionLevel = Program.LoggedInUsers[uId].Jogosultsag;
+
+                        var service = await context.Services.FindAsync(serviceId);
+                        if (service == null)
+                        {
+                            return NotFound("A szolgáltatás nem található.");
+                        }
+
+                        if (service.UserId != userId && userPermissionLevel != 9)
+                        {
+                            return Forbid("Nincs jogosultságod ennek a szolgáltatásnak a törlésére.");
+                        }
+
+
+                        var userServices = context.UserServices.Where(us => us.ServiceId == serviceId);
+                        context.UserServices.RemoveRange(userServices);
+                        context.Services.Remove(service);
+                        await context.SaveChangesAsync();
+
+                        return Ok("Szolgáltatás sikeresen törölve.");
                     }
-
-                    var loggedInUser = Program.LoggedInUsers[uId];
-                    int userId = Program.LoggedInUsers[uId].UserId;
-                    int userPermissionLevel = Program.LoggedInUsers[uId].Jogosultsag;
-
-                    var service = await context.Services.FindAsync(serviceId);
-                    if (service == null)
+                    catch (Exception ex)
                     {
-                        return NotFound("A szolgáltatás nem található.");
+                        return BadRequest(ex.Message);
                     }
-
-                    if (service.UserId != userId && userPermissionLevel != 9)
-                    {
-                        return Forbid("Nincs jogosultságod ennek a szolgáltatásnak a törlésére.");
-                    }
-
-                    
-                    var userServices = context.UserServices.Where(us => us.ServiceId == serviceId);
-                    context.UserServices.RemoveRange(userServices);
-                    context.Services.Remove(service);
-                    await context.SaveChangesAsync();
-
-                    return Ok("Szolgáltatás sikeresen törölve.");
                 }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
+
             }
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateService(int id,Service updatedService, string uId)
-        {
-            using (var context = new VizsgaremekContext())
+        
+            [HttpPut("{id}")]
+            public async Task<IActionResult> UpdateService(int id, Service updatedService, string uId)
             {
-                try
+                using (var context = new VizsgaremekContext())
                 {
-                    if (id != updatedService.ServiceId)
+                    try
                     {
-                        return BadRequest("ID mismatch.");
-                    }
+                        if (id != updatedService.ServiceId)
+                        {
+                            return BadRequest("ID mismatch.");
+                        }
 
-                    var existingService = await context.Services.FindAsync(id);
-                    if (existingService == null)
+                        var existingService = await context.Services.FindAsync(id);
+                        if (existingService == null)
+                        {
+                            return NotFound();
+                        }
+
+                        int userID = Program.LoggedInUsers[uId].UserId;
+                        int jog = Program.LoggedInUsers[uId].Jogosultsag;
+
+                        if (existingService.UserId != userID && jog != 9)
+                        {
+                            return Unauthorized("Ez nem a te szolgáltatásod!");
+                        }
+
+
+                        existingService.ServiceName = updatedService.ServiceName;
+                        existingService.TimeCost = updatedService.TimeCost;
+                        existingService.Description = updatedService.Description;
+                        existingService.CategoryId = updatedService.CategoryId;
+                        existingService.CreatedAt = updatedService.CreatedAt;
+
+                        await context.SaveChangesAsync();
+
+                        return Ok(existingService);
+                    }
+                    catch (Exception ex)
                     {
-                        return NotFound();
+                        return BadRequest(ex.Message);
                     }
-
-                    int userID = Program.LoggedInUsers[uId].UserId;
-                    int jog = Program.LoggedInUsers[uId].Jogosultsag;
-
-                    if (existingService.UserId != userID && jog != 9)
-                    {
-                        return Unauthorized("Ez nem a te szolgáltatásod!");
-                    }
-
-                    
-                    existingService.ServiceName = updatedService.ServiceName;
-                    existingService.TimeCost = updatedService.TimeCost;
-                    existingService.Description = updatedService.Description;
-                    existingService.CategoryId = updatedService.CategoryId;
-                    existingService.CreatedAt = updatedService.CreatedAt;
-
-                    await context.SaveChangesAsync();
-
-                    return Ok(existingService);
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
                 }
             }
+
         }
-
-    }
-}
-
+    } 
 
 
 
