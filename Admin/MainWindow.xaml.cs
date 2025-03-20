789e993ffed532e16a69d142d5ffd2ea65f56ecd
budahaizm <budahazim@kkszki.hu>
2025-03-20 10:30:00 +0100

    b

--- Admin/MainWindow.xaml.cs
+++ Admin/MainWindow.xaml.cs
@@ -14,9 +14,7 @@ using System.Windows.Shapes;
 
 namespace Admin
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
 
